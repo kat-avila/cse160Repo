d9ff8967f25c2f26ff9ea0c2b1d79341d49b8245
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-03 16:17:03 -0700

    Redered blue rectangle

---
+++ Katia_Avila_Pinedo_Assigment_0.zip/asg0.js
@@ -0,0 +1,17 @@
+// asg0.js for asg0.html
+function main() {  
+    // Retrieve <canvas> element
+    var canvas = document.getElementById('example');  
+    if (!canvas) { 
+      console.log('Failed to retrieve the <canvas> element');
+      return false; 
+    } 
+  
+    // Get the rendering context for 2DCG
+    var ctx = canvas.getContext('2d');
+  
+    // Draw a blue rectangle
+    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; // Set color to blue
+    ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+  }
+  
