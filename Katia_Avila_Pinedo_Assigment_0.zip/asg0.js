152752d52c4021fa98b34541b89c7738273886d5
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-05 13:03:35 -0700

    Completed step 2, black canvas with red vector

--- Katia_Avila_Pinedo_Assigment_0.zip/asg0.js
+++ Katia_Avila_Pinedo_Assigment_0.zip/asg0.js
@@ -9,19 +9,23 @@ function main() {
 
     // Get the rendering context for 2DCG
     var ctx = canvas.getContext('2d');
-    
+    // setup a black canvas
+    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
+    ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
+    var rectOrigin = [canvas.width/2 , canvas.height/2 ]; // offset + (1/2 * width or height)
+    console.log(canvas.width, canvas.height, rectOrigin[0], rectOrigin[1]);
 
-    // Draw a black rectangle
-    ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to black
-    ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
-    var rectOrigin = [120 + (150/2), 10 + (150/2)];
+    // Draw a blue rectangle
+    // ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
+    // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
+    // var rectOrigin = [120 + (150/2), 10 + (150/2)]; // offset + (1/2 * width or height)
     
     // instantiate vector v1
     let v1 = new Vector3();
-    v1[0] = 2.25 * 0.20; //x coordinate, scaled by 20
-    v1[1] = 2.25 * 0.20; //y coordinate, scaled by 20
+    v1[0] = 2.25 * 20; //x coordinate, scaled by 20
+    v1[1] = 2.25 * 20; //y coordinate, scaled by 20
     v1[2] = 0; //z
-    console.log(v1);
+    // console.log(v1);
 
     // draw vector in 2D
     function drawVector(v, color) {
@@ -31,11 +35,12 @@ function main() {
         ctx.strokeStyle = color;
         //start a new path
         ctx.beginPath();
-        var x = v1[0];
-        var y = v1[1];
+        var x = rectOrigin[0] + v1[0];
+        var y = rectOrigin[1] + (-1 * v1[1]); //adjust for opposing direction in coordinate system vs canvas
 
         ctx.moveTo(rectOrigin[0], rectOrigin[1]); // start at origin of rectangle in canvas
-        ctx.lineTo(x , y ); // scaled by 20
+        // draw vector 
+        ctx.lineTo(x, y); 
         ctx.stroke();
     }
 
