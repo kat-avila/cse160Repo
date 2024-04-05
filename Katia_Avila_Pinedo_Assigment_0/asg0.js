11045590454c17a59320eaad6447e914adec58cd
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-05 14:58:08 -0700

    Completed part3: Dynamic values for red v1 vector

--- Katia_Avila_Pinedo_Assigment_0/asg0.js
+++ Katia_Avila_Pinedo_Assigment_0/asg0.js
@@ -19,13 +19,9 @@ function main() {
     // ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
     // ctx.fillRect(120, 10, 150, 150);        // Fill a rectangle with the color
     // var rectOrigin = [120 + (150/2), 10 + (150/2)]; // offset + (1/2 * width or height)
-    
+                
     // instantiate vector v1
     let v1 = new Vector3();
-    v1[0] = 2.25 * 20; //x coordinate, scaled by 20
-    v1[1] = 2.25 * 20; //y coordinate, scaled by 20
-    v1[2] = 0; //z
-    // console.log(v1);
 
     //handle draw event
     drawButton.onclick = function handleDrawEvent() {
@@ -37,13 +33,19 @@ function main() {
         ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set color to blue
         ctx.fillRect(0, 0, canvas.width, canvas.height);        // Fill a rectangle with the color
 
-
         //read values of text boxes
-        // let v1xCord = document.getElementById('v1xCord');
-        // console.log("v1xcord", v1xCord);
+         let v1xCord = document.getElementById('v1xCord').value;
+        //  console.log("v1xcord", v1xCord);
+         let v1yCord = document.getElementById('v1yCord').value;
+        //  console.log("v1ycord", v1yCord);
+
+        //set coordinates
+        v1[0] = v1xCord * 20; //x coordinate, scaled by 20
+        v1[1] = v1yCord* 20; //y coordinate, scaled by 20
+        v1[2] = 0; //z
 
         // call draw vector(v1, "red")
-        // drawVector(v1, "red");
+        drawVector(v1, "red");
         return false;
     }
 
@@ -61,9 +63,6 @@ function main() {
         ctx.lineTo(x, y); 
         ctx.stroke();
     }
-
-    drawVector(v1, "red");
-
 
 }
 
