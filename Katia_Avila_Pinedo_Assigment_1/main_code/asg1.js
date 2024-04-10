e8863fce0fd5c4605acbb791f18e1a08ada4bab8
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-10 14:10:09 -0700

    Added clear button

--- Katia_Avila_Pinedo_Assigment_1/main_code/asg1.js
+++ Katia_Avila_Pinedo_Assigment_1/main_code/asg1.js
@@ -74,6 +74,7 @@ function addActionsForHTMLUI() {
   // Button Events (Shape Type)
   document.getElementById('green').onclick = function () { g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
   document.getElementById('red').onclick = function () { g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+  document.getElementById('clear').onclick = function () { g_shapesList = []; renderAllShapes();};
 
   // Color Slider Events
   document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100})
