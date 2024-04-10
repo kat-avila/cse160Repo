d8aa106e13afb3be5004c750e6d133f84c1fcd1b
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-09 22:22:37 -0700

    Draw shape on click

--- Katia_Avila_Pinedo_Assigment_1/main_code/asg1.js
+++ Katia_Avila_Pinedo_Assigment_1/main_code/asg1.js
@@ -1,10 +1,12 @@
-// HelloPoint1.js (c) 2012 matsuda
+// ClickedPoints.js (c) 2012 matsuda
 // modifed for asg1.html Katia Avila Pinedo 4-2024
 // Vertex shader program
-var VSHADER_SOURCE = 
+var VSHADER_SOURCE =
+  'attribute vec4 a_Position;\n' +  //variable for position
+  'attribute float a_PointSize;\n' + // variable for size of shape
   'void main() {\n' +
-  '  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n' + // Set the vertex coordinates of the point
-  '  gl_PointSize = 10.0;\n' +                    // Set the point size
+  '  gl_Position = a_Position;\n' + // Set the vertex coordinates of the point
+  '  gl_PointSize = a_PointSize;\n' +                    // Set the point size
   '}\n';
 
 // Fragment shader program
@@ -30,12 +32,49 @@ function main() {
     return;
   }
 
+  // Get the storage location of attribute variable
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if (a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return;
+  }
+  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  // Set vertex position and size to attribute variable
+  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+  gl.vertexAttrib1f(a_PointSize, 5.0);
+
+  // Registed function (event hanler) to be called on a mouse press
+  canvas.onmousedown = function (ev) { click(ev, gl, canvas, a_Position); };
+
   // Specify the color for clearing <canvas>
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
+}
 
-  // Draw a point
-  gl.drawArrays(gl.POINTS, 0, 1);
+// Draw a point
+// gl.drawArrays(gl.POINTS, 0, 1);
+var g_points = []; // The array for the position of a mouse press, and previous positions to be drawn
+function click(ev, gl, canvas, a_Position) {
+  var x = ev.clientX; // x coordinate of a mouse pointer
+  var y = ev.clientY; // y coordinate of a mouse pointer
+  var rect = ev.target.getBoundingClientRect(); // gets positions of canvas in client area
+
+  x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
+  y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
+  // Store the coordinates to g_points array
+  g_points.push(x); g_points.push(y);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  var len = g_points.length; // render all past clicks
+  for (var i = 0; i < len; i += 2) {
+    // Pass the position of a point to a_Position variable
+    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+
+    // Draw a point
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
 }
