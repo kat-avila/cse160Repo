9356b9a8862b9b52e3ff68c5f2eda1f6bdd2de4b
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-19 18:48:12 -0700

    multiple textures

--- Katia_Avila_Pinedo_Assigment_5A/main_code/asg5A.js
+++ Katia_Avila_Pinedo_Assigment_5A/main_code/asg5A.js
@@ -15,27 +15,82 @@ function main() {
     camera.position.z = 2;
 
     const scene = new THREE.Scene();
+    // add lighting
+    const color = 0xFFFFFF;
+    const intensity = 2;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+
     // creatre box geometry
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    // create material
-    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+
+    // // create texture loader
+    // const loader = new THREE.TextureLoader();
+    // const texture = loader.load('../lib/evilMorty.jpg');
+    // texture.colorSpace = THREE.SRGBColorSpace;
+
+    function loadColorTexture(path) {
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load(path);
+        texture.colorSpace = THREE.SRGBColorSpace;
+        return texture;
+    }
+    
+    // add more cubes
+    function makeInstance(geometry, color, x) {
+        // const material = new THREE.MeshPhongMaterial({ color });
+        // create material
+        // const material = new THREE.MeshPhongMaterial({ 
+        //     // color: 0x44aa88 
+        //     map: texture,
+        // }); 
+        const materials = [
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/evilMorty.jpg') }),
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/melting.jpg') }),
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/negSunset.jpg') }),
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/susRick.jpg') }),
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/trippingFade.jpg') }),
+            new THREE.MeshBasicMaterial({ map: loadColorTexture('../lib/vintage.jpg') }),
+        ];
+    
+        // const cube = new THREE.Mesh(geometry, material);
+        const cube = new THREE.Mesh(geometry, materials);
+        scene.add(cube);
+    
+        cube.position.x = x;
+    
+        return cube;
+    }
+        
     // create mesh = geomtery + material + position/orientation/scale
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube); // add mesh to scene
+    // const cube = new THREE.Mesh(geometry, material);
+    const cubes = [
+        makeInstance(geometry, 0x44aa88, 0),
+        makeInstance(geometry, 0x8844aa, -2),
+        makeInstance(geometry, 0xaa8844, 2),
+    ];
+
     //render the scene
     function render(time) {
         time *= 0.001;  // convert time to seconds
-    
-        cube.rotation.x = time;
-        cube.rotation.y = time;
-    
+
+        // cube.rotation.x = time;
+        // cube.rotation.y = time;
+        cubes.forEach((cube, ndx) => {
+            const speed = 1 + ndx * .1;
+            const rot = time * speed;
+            cube.rotation.x = rot;
+            cube.rotation.y = rot;
+        });
+
         renderer.render(scene, camera);
-    
+
         requestAnimationFrame(render); // request to animate
     }
     requestAnimationFrame(render);
-    }
+}
 
