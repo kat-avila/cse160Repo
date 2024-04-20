a71cffe756f79acd5236c895cd4a841d5bf30df1
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-19 18:12:50 -0700

    moved js to new file

--- Katia_Avila_Pinedo_Assigment_5A/main_code/asg5A.js
+++ Katia_Avila_Pinedo_Assigment_5A/main_code/asg5A.js
@@ -1,29 +1,30 @@
- //import * as THREE from './three.module.js';
-// import WebGL from './three/addons/capabilities/WebGL.js';
-// import { OBJLoader } from './three/addons/loaders/OBJLoader.js';
-
+import * as THREE from "three";
 function main() {
-	const canvas = document.querySelector( '#c' );
-	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
-	const fov = 75;
-	const aspect = 2; // the canvas default
-	const near = 0.1;
-	const far = 5;
-	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+    // create perspective camera
+    const fov = 75; // field of view, 75 deg in vert
+    const aspect = 2; // 300x150 the canvas default
+    const near = 0.1; //fustrum start
+    const far = 5;  //fustrum end
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    // cube at origin, give space to observe
+    camera.position.z = 2;
 
-	const scene = new THREE.Scene();
-
-	const boxWidth = 1;
-	const boxHeight = 1;
-	const boxDepth = 1;
-	const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
-
-	const material = new THREE.MeshBasicMaterial( { color: 0x44aa88 } ); // greenish blue
-
-	const cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-
-	renderer.render( scene, camera );
+    const scene = new THREE.Scene();
+    // creatre box geometry
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    // create material
+    const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 }); // greenish blue
+    // create mesh = geomtery + material + position/orientation/scale
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube); // add mesh to scene
+    //render the scene
+    renderer.render(scene, camera);
 }
+
+main();
