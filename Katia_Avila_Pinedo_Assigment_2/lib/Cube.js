5a57127b65b0704f918afc711a0a08f75ed3033e
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-22 19:30:04 -0700

    Shades on different faces

--- Katia_Avila_Pinedo_Assigment_2/lib/Cube.js
+++ Katia_Avila_Pinedo_Assigment_2/lib/Cube.js
@@ -10,7 +10,7 @@ class Cube {
         var rgba = this.color;
         // var size = this.size;
 
-        // Pass the color of a point to u_FragColor variable
+        // Pass the color of a point to u_FragColor uniform variable
         gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
 
         // Pass the matrix to u_ModelMatrix attribute
@@ -19,7 +19,9 @@ class Cube {
         // Front of cube
         drawTriangle3D([0.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,0.0]);
         drawTriangle3D([0.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0]);
-        // Top of cube
+        // faux lighting
+        gl.uniform4f(u_FragColor, rgba[0] * 0.9, rgba[1] * 0.9, rgba[2] * 0.9, rgba[3]);
+        // right side of cube
         drawTriangle3D([1.0,0.0,0.0, 1.0,1.0,0.0, 1.0,0.0,1.0]);  
         drawTriangle3D([1.0,1.0,0.0, 1.0,0.0,1.0, 1, 1, 1]);  
 
--- Katia_Avila_Pinedo_Assigment_2/main_code/asg2.js
+++ Katia_Avila_Pinedo_Assigment_2/main_code/asg2.js
@@ -113,7 +113,7 @@ function addActionsForHTMLUI() {
   document.getElementById('alphaSlide').addEventListener('mouseup', function () { g_selectedColor[3] = this.value / 100 })
 
   // Size Slider Events
-  document.getElementById('angleSlide').addEventListener('mouseup', function () { g_globalAngle = this.value; renderAllShapes(); })
+  document.getElementById('angleSlide').addEventListener('mousemove', function () { g_globalAngle = this.value; renderAllShapes(); })
   document.getElementById('segmentSlide').addEventListener('mouseup', function () { g_selectedSegment = this.value })
 }
 
@@ -194,24 +194,17 @@ function renderAllShapes() {
   // COLLARBONE cube
   var collar = new Cube();
   collar.color = [0.83, 0.71, 0.59, 1.0];
-  // collar.matrix.translate(-0.35, 0.3, 0.0);
-  collar.matrix.scale(0.5, 0.5, 0.5);
+  collar.matrix.translate(-0.35, 0.3, 0.0);
+  collar.matrix.scale(0.75, 0.15, .5);
   collar.render();
 
-  // // COLLARBONE cube
-  // var collar = new Cube();
-  // collar.color = [0.83, 0.71, 0.59, 1.0];
-  // collar.matrix.translate(-0.35, 0.3, 0.0);
-  // collar.matrix.scale(0.75, 0.15, .5);
-  // collar.render();
-
-  // // left arm, top section cube
-  // var armLT = new Cube();
-  // armLT.color = [0.83, 0.71, 0.59, 1.0];
-  // armLT.matrix.translate(-.4, 0.2, 0.0);
-  // armLT.matrix.rotate(45, 0 ,0 ,1);
-  // armLT.matrix.scale(0.1, 0.15, .1);
-  // armLT.render();
+  // left arm, top section cube
+  var armLT = new Cube();
+  armLT.color = [0.83, 0.71, 0.59, 1.0];
+  armLT.matrix.translate(-.4, 0.2, 0.0);
+  armLT.matrix.rotate(45, 0 ,0 ,1);
+  armLT.matrix.scale(0.1, 0.15, 0.1);
+  armLT.render();
 
   
 }
