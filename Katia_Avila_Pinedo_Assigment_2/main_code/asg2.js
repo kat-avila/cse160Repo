bf8a56a4116c80227d9c97f10038e0237151d24b
Katia Avila Pinedo <59909415+kat-avila@users.noreply.github.com>
2024-04-24 01:14:37 -0700

    Added belt, whole butt and buldge

--- Katia_Avila_Pinedo_Assigment_2/main_code/asg2.js
+++ Katia_Avila_Pinedo_Assigment_2/main_code/asg2.js
@@ -4,9 +4,11 @@
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
   uniform mat4 u_ModelMatrix;
-  uniform mat4 u_GlobalRotateMatrix;
+  uniform mat4 u_XRotateMatrix;
+  uniform mat4 u_YRotateMatrix;
+  uniform mat4 u_ZRotateMatrix;
   void main() { 
-    gl_Position = u_GlobalRotateMatrix * u_ModelMatrix * a_Position;
+    gl_Position = u_XRotateMatrix * u_YRotateMatrix * u_ZRotateMatrix * u_ModelMatrix * a_Position;
   }`
 // Fragment shader program
 var FSHADER_SOURCE = `
@@ -22,7 +24,10 @@ let gl;
 let a_Position;
 let u_FragColor;
 let u_ModelMatrix;
-let u_GlobalRotateMatrix;
+let u_XRotateMatrix;
+let u_YRotateMatrix;
+let u_ZRotateMatrix;
+
 
 // Setup WebGL
 function setupWebGL() {
@@ -65,12 +70,24 @@ function connectVariablesToGLSL() {
     return;
   }
 
-    // Get the storage location of u_GlobalRotateMatrix
-    u_GlobalRotateMatrix = gl.getUniformLocation(gl.program, 'u_GlobalRotateMatrix');
-    if (!u_GlobalRotateMatrix) {
-      console.log('Failed to get the storage location of u_GlobalRotateMatrix');
-      return;
-    }
+  // Get the storage location of u_XRotateMatrix
+  u_XRotateMatrix = gl.getUniformLocation(gl.program, 'u_XRotateMatrix');
+  if (!u_XRotateMatrix) {
+    console.log('Failed to get the storage location of u_XRotateMatrix');
+    return;
+  }
+  // Get the storage location of u_YRotateMatrix
+  u_YRotateMatrix = gl.getUniformLocation(gl.program, 'u_YRotateMatrix');
+  if (!u_YRotateMatrix) {
+    console.log('Failed to get the storage location of u_YRotateMatrix');
+    return;
+  }
+  // Get the storage location of u_ZRotateMatrix
+  u_ZRotateMatrix = gl.getUniformLocation(gl.program, 'u_ZRotateMatrix');
+  if (!u_ZRotateMatrix) {
+    console.log('Failed to get the storage location of u_ZRotateMatrix');
+    return;
+  }
 
   //set an initial value for this matrix indentity
   var indentityM = new Matrix4();
@@ -78,7 +95,9 @@ function connectVariablesToGLSL() {
 }
 
 // Globals related to UI elments
-let g_globalAngle = 0; // camera angle
+let g_angleX = 25; // camera angle
+let g_angleY = 0; // camera angle
+let g_angleZ = 0; // camera angle
 let g_rightTopAngle = 0; // angle top right arm
 let g_rightBottomAngle = 0; // angle bottom right
 let g_leftTopAngle = 0; // angle top left arm
@@ -86,10 +105,12 @@ let g_leftBottomAngle = 0; // angle bottom left
 // animations
 let g_thrustAnimation = false;
 
-function addActionsForHTMLUI() {  
+function addActionsForHTMLUI() {
   // Perspective Slider Events
-  document.getElementById('angleSlide').addEventListener('mousemove', function () { g_globalAngle = this.value; renderAllShapes(); })
-  
+  document.getElementById('angleXslide').addEventListener('mousemove', function () { g_angleX = this.value; renderAllShapes(); })
+  document.getElementById('angleYslide').addEventListener('mousemove', function () { g_angleY = this.value; renderAllShapes(); })
+  document.getElementById('angleZslide').addEventListener('mousemove', function () { g_angleZ = this.value; renderAllShapes(); })
+
   // Right Arm Slider Events
   document.getElementById('rightTopSlide').addEventListener('mousemove', function () { g_rightTopAngle = this.value; renderAllShapes(); })
   document.getElementById('rightBottomSlide').addEventListener('mousemove', function () { g_rightBottomAngle = this.value; renderAllShapes(); })
@@ -98,8 +119,8 @@ function addActionsForHTMLUI() {
   document.getElementById('leftBottomSlide').addEventListener('mousemove', function () { g_leftBottomAngle = this.value; renderAllShapes(); })
 
   // Thrust buttons
-  document.getElementById('thrustStart').onclick = function () {g_thrustAnimation = true; };
-  document.getElementById('thrustStop').onclick = function () {g_thrustAnimation = false; };
+  document.getElementById('thrustStart').onclick = function () { g_thrustAnimation = true; };
+  document.getElementById('thrustStop').onclick = function () { g_thrustAnimation = false; };
 
 }
 
@@ -121,11 +142,11 @@ function main() {
   requestAnimationFrame(tick);
 }
 
-var g_startTime = performance.now()/1000.0;
-var g_seconds = (performance.now()/1000.0) - g_startTime;
+var g_startTime = performance.now() / 1000.0;
+var g_seconds = (performance.now() / 1000.0) - g_startTime;
 function tick() {
   // Save the current time
-  g_seconds = (performance.now() / 1000.0 )- g_startTime;
+  g_seconds = (performance.now() / 1000.0) - g_startTime;
   // Update animation angles
   updateAnimationAngles();
 
@@ -148,13 +169,17 @@ function renderAllShapes() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   // Pass matrix to u_ModelMatrix attribute
-  var globalRotMat = new Matrix4().rotate(g_globalAngle, 0, 1,0);
-  gl.uniformMatrix4fv(u_GlobalRotateMatrix, false, globalRotMat.elements);
-
+  var xAngMat = new Matrix4().rotate(g_angleX, 0, 1, 0);
+  gl.uniformMatrix4fv(u_XRotateMatrix, false, xAngMat.elements);
+  var yAngMat = new Matrix4().rotate(g_angleY, 1, 0, 0);
+  gl.uniformMatrix4fv(u_YRotateMatrix, false, yAngMat.elements);
+  var zAngMat = new Matrix4().rotate(g_angleZ, 0, 0, 1);
+  gl.uniformMatrix4fv(u_ZRotateMatrix, false, zAngMat.elements);
 
   // main body    torso.color = [0.91, 0.8, 0.69, 1.0];
   // tan body    pecR.color = [0.83, 0.71, 0.59, 1.0];
   // dark skin    [0.66, 0.52, 0.37, 1.0]
+  // underpands  [0.71, 0.21, 0.16, 1.0];
 
   // TORSO
   var torso = new Cube();
@@ -163,6 +188,31 @@ function renderAllShapes() {
   var torsoCoordMatrix = new Matrix4(torso.matrix);
   torso.matrix.scale(0.75, 1.36, .2);
   torso.render();
+  // BELT
+  var belt = new Cube();
+  belt.color = [0.84, 0.77, 0.38, 1.0];
+  belt.matrix.set(torsoCoordMatrix);
+  belt.matrix.translate(-0.03, -0.05, 0.01);
+  belt.matrix.scale(1, 0.25, .3);
+  var beltCoordMatrix = new Matrix4(belt.matrix);
+  belt.render();
+  // BUTT ALL
+  var buttWhole = new Cube();
+  buttWhole.color = [0.91, 0.8, 0.69, 1.0];
+  buttWhole.matrix.set(beltCoordMatrix);
+  buttWhole.matrix.translate(0.0, -0.5, 0);
+  buttWhole.matrix.scale(1, 2, 1);
+  var buttWholeCoordMatrix = new Matrix4(buttWhole.matrix);
+  buttWhole.render();
+   // BULDGE 
+   var buldge = new Cube();
+   buldge.color = [0.71, 0.21, 0.16, 1.0];
+   buldge.matrix.set(buttWholeCoordMatrix);
+   buldge.matrix.translate(0.09, 0, -0.25);
+   buldge.matrix.scale(0.3, 1, 0.5);
+   buldge.render();
+
+
 
   // COLLARBONE cube
   var collar = new Cube();
@@ -183,36 +233,30 @@ function renderAllShapes() {
   armRT.render();
   // right arm, bottom section 
   var armRB = new Cube();
-  armRB.color =[0.37, 0.63, 0.5, 1];
+  armRB.color = [0.37, 0.63, 0.5, 1];
   armRB.matrix.set(rightArmCoordMart);
   armRB.matrix.translate(0.23, 0.05, 0.0001);
   armRB.matrix.rotate(-g_rightBottomAngle, 0, 0, 1);
-  // if (g_thrustAnimation) {
-  //   armRB.matrix.rotate(-35 * Math.sin(g_seconds), 0, 0, 1);
-  // } else { 
-  //   armRB.matrix.rotate(-g_rightBottomAngle, 0, 0, 1);
-  // }
-  console.log(Math.sin(g_seconds));
   armRB.matrix.scale(0.2, 1.1, 0.2);
   armRB.render();
 
-  
+
   // left arm, top section
   var armLT = new Cube();
   armLT.color = [0.24, 0.33, 0.29, 1.0];
   armLT.matrix.set(torsoCoordMatrix);
   armLT.matrix.translate(-0.041, 0.34, 0.001);
-  armLT.matrix.rotate( 90, 0, 0, 1);
+  armLT.matrix.rotate(90, 0, 0, 1);
   armLT.matrix.rotate(g_leftTopAngle, 0, 0, 1);
   var leftArmCoordMart = new Matrix4(armLT.matrix);
   armLT.matrix.scale(0.2, 1.1, 0.2);
   armLT.render();
   // left arm, bottom section 
   var armLB = new Cube();
-  armLB.color =[0.37, 0.63, 0.5, 1];
+  armLB.color = [0.37, 0.63, 0.5, 1];
   armLB.matrix.set(leftArmCoordMart);
   armLB.matrix.translate(0, 0.27, 0.0001);
-  armLB.matrix.rotate( -90, 0, 0, 1);
+  armLB.matrix.rotate(-90, 0, 0, 1);
   armLB.matrix.rotate(g_leftBottomAngle, 0, 0, 1);
   armLB.matrix.scale(0.2, 1.2, 0.2);
   armLB.render();
@@ -237,10 +281,10 @@ function renderAllShapes() {
 
   // left pec nip
   var pecNipL = new Cube();
-  pecNipL.color =  [0.66, 0.52, 0.37, 1.0];
+  pecNipL.color = [0.66, 0.52, 0.37, 1.0];
   pecNipL.matrix.set(leftPecMatrix);
   pecNipL.matrix.translate(0.1, 0.05, -0.25);
-  pecNipL.matrix.rotate(45, 0 ,0 ,1);
+  pecNipL.matrix.rotate(45, 0, 0, 1);
   pecNipL.matrix.scale(0.3, 0.3, 0.25);
   pecNipL.render();
   // right pec nip
@@ -248,9 +292,9 @@ function renderAllShapes() {
   pecNipR.color = [0.66, 0.52, 0.37, 1.0];
   pecNipR.matrix.set(rightPecMatrix);
   pecNipR.matrix.translate(0.15, 0.05, -0.25);
-  pecNipR.matrix.rotate(45, 0 ,0 ,1);
+  pecNipR.matrix.rotate(45, 0, 0, 1);
   pecNipR.matrix.scale(0.3, 0.3, 0.25);
   pecNipR.render();
-   
-  
+
+
 }
